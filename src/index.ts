c637ffb910b882dc5e3121640cbe98c8e7f1fcd5
Adam Simon <adamsimon488@gmail.com>
2023-08-06 18:47:29 +0200

    Added selection sort

--- src/index.ts
+++ src/index.ts
@@ -188,4 +188,22 @@ function timSort(arr: number[]) {
   }
 }
 
+function selectionSort(arr: number[]) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+}
 
