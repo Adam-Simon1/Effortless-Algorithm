32fe523f2c1f63dda0662ebe235db72f4452ccd9
Adam Simon <adamsimon488@gmail.com>
2023-08-06 18:47:53 +0200

    Added shell sort

--- src/index.ts
+++ src/index.ts
@@ -207,3 +207,24 @@ function selectionSort(arr: number[]) {
   }
 }
 
+function shellSort(arr: number[]) {
+  const n = arr.length;
+  let gap = Math.floor(n / 2);
+
+  while (gap > 0) {
+    for (let i = gap; i < n; i++) {
+      const temp = arr[i];
+      let j = i;
+
+      while (j >= gap && arr[j - gap] > temp) {
+        arr[j] = arr[j - gap];
+        j -= gap;
+      }
+
+      arr[j] = temp;
+    }
+
+    gap = Math.floor(gap / 2);
+  }
+}
+
